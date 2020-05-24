1b96af46f3a4646745a31e51e18cc45c60efb68f
Yannick KIKI <seyive.kiki@gmail.com>
2020-05-24 10:26:21 +0100

    - Finish tutorial part 2: implement a chat server

--- Chat/settings.py
+++ Chat/settings.py
@@ -124,3 +124,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 ASGI_APPLICATION = "Chat.routing.application"
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('127.0.0.1', 6379)],
+        },
+    },
+}
